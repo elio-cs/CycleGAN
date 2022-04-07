0c68a81a54a6e7321410ae8578fbadbc870f6898
XinyuZhang <githubzxy@gmail.com>
2022-04-07 14:15:22 +0800

    Update config.py

--- config.py
+++ config.py
@@ -17,3 +17,13 @@ CHECKPOINT_GEN_H = "genh.pth.tar"
 CHECKPOINT_GEN_Z = "genz.pth.tar"
 CHECKPOINT_CRITIC_H = "critich.pth.tar"
 CHECKPOINT_CRITIC_Z = "criticz.pth.tar"
+
+transforms = A.Compose(
+    [
+        A.Resize(width = 256, height = 256),
+        A.HorizontalFlip(p = 0.5),
+        A.Normalize(mean = [0.5, 0.5, 0.5], std = [0.5, 0.5, 0.5], max_pixel_value = 255),
+        ToTensorV2(),
+    ],
+    additional_targets={"image0": "image"}
+)
