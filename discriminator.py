3fa4aa55e3d97d0fa898ea49d47662b549298203
XinyuZhang <githubzxy@gmail.com>
2022-04-07 10:12:09 +0800

    Update discriminator.py

--- discriminator.py
+++ discriminator.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 class Block(nn.Module):
 	def __init__(self, in_channels, out_channels, stride):
 		super().__init__()
-		self.conv == nn.Sequential(
+		self.conv = nn.Sequential(
 			nn.Conv2d(in_channels, out_channels, 4, stride, 1, bias=True, padding_mode="reflect"),
 			nn.InstanceNorm2d(out_channels),
 			nn.leakyRelu(0.2),
