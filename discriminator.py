34a144b82569cc3a95385f0c534ff7746be2b739
XinyuZhang <githubzxy@gmail.com>
2022-04-07 10:15:07 +0800

    Update discriminator.py

--- discriminator.py
+++ discriminator.py
@@ -8,7 +8,7 @@ class Block(nn.Module):
 		self.conv = nn.Sequential(
 			nn.Conv2d(in_channels, out_channels, 4, stride, 1, bias=True, padding_mode="reflect"),
 			nn.InstanceNorm2d(out_channels),
-			nn.leakyReLU(0.2),
+			nn.LeakyReLU(0.2),
 		)
 
 	def forward(self, x):
