bf8300653f4ab1453b2c4c32a0e9a70ff15562a7
XinyuZhang <githubzxy@gmail.com>
2022-04-08 17:15:26 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,3 +1,5 @@
+from operator import truediv
+from random import shuffle
 import torch
 from dataset import HorseZebraDataset
 import sys
@@ -11,3 +13,65 @@ from torchvision.utils import save_image
 from discriminator import Discriminator
 from generator import Generator
 
+def train_fn():
+    pass
+
+def main():
+    disc_H = Discriminator(in_channels=3).to(config.DEVICE)
+    disc_Z = Discriminator(in_channels=3).to(config.DEVICE)
+    gen_H = Generator(img_channels=3, num_residuals=9).to(config.DEVICE)
+    gen_Z = Generator(img_channels=3, num_residuals=9).to(config.DEVICE)
+    opt_disc = optim.Adam(
+        list(disc_H.parameters()) + list(disc_Z.parameters()),
+        lr = config.config.LEARNING_RATE,
+        betas = (0.5, 0.999),
+    )
+    opt_gen = optim.Adam(
+        list(gen_H.parameters()) + list(gen_Z.parameters()),
+        lr = config.config.LEARNING_RATE,
+        betas = (0.5, 0.999),
+    )
+
+    L1 = nn.L1Loss()
+    mse = nn.MSELoss()
+
+    if config.LOAD_MODEL:
+        load_checkpoint(
+            config.CHECKPOINT_GEN_H, gen_H, opt_gen, config.LEARNING_RATE,
+        )
+        load_checkpoint(
+            config.CHECKPOINT_GEN_Z, gen_Z, opt_gen, config.LEARNING_RATE,
+        )
+        load_checkpoint(
+            config.CHECKPOINT_CRITIC_H, disc_H, opt_disc, config.LEARNING_RATE,
+        )
+        load_checkpoint(
+            config.CHECKPOINT_CRITIC_Z, disc_Z, opt_disc, config.LEARNING_RATE,
+        )
+    
+    dataset = HorseZebraDataset(
+        root_horse=config.Train_Dir+"/horse", root_zebra=config.Train_Dir+"/zebra", transform=config.transforms
+    )
+
+    loader = DataLoader(
+        dataset,
+        batch_size = config.BATCH_SIZE,
+        shuffle=True,
+        num_workers=config.NUM_WORKERS
+        pin_memory=True
+    )
+
+    g_scaler = torch.cuda.amp.GradScaler()
+    d_scaler = torch.cuda.amp.GradScaler()
+
+    for epoch in range(config.NUM_EPOCHS):
+        train_fn(disc_H, disc_Z, gen_H, gen_Z, loader, opt_disc, opt_gen, L1, mse, d_scaler, g_scaler)
+
+        if config.SAVE_MODEL:
+            save_checkpoint(gen_H, opt_gen, filename=config.CHECKPOINT_GEN_H)
+            save_checkpoint(gen_Z, opt_gen, filename=config.CHECKPOINT_GEN_Z)
+            save_checkpoint(disc_H, opt_gen, filename=config.CHECKPOINT_CRITIC_H)
+            save_checkpoint(disc_Z, opt_gen, filename=config.CHECKPOINT_CRITIC_Z)
+
+if __name__ == "__main__":
+    main()
